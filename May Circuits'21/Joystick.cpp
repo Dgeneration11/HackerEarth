74ad96684124ef55b5866fc0574b6dec72c6f7ad
Bhupendra Gehlot <buntydgeneration@gmail.com>
2021-05-20 15:52:38 +0530

    new

---
+++ May Circuits'21/Joystick.cpp
@@ -0,0 +1,24 @@
+#include<bits/stsc++.h>
+using namespace std;
+
+int main(){
+	for (int i=1;i<5;i++){
+		cout<<"0 "<<i<<endl;
+	}
+
+	for (int i=5;i>=0;i--){
+		cout<<"1 "<<i<<endl;
+	}
+
+	for (int i=0;i<6;i++){
+		cout<<"2 "<<i<<endl;
+	}
+	
+	for (int i=5;i>=0;i--){
+		cout<<"3 "<<i<<endl;
+	}
+
+	for (int i=1;i<5;i++){
+		cout<<"4 "<<i<<endl;
+	}
+}
